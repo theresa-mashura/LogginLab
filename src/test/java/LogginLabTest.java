9eb14ed409975a93f57859e459694b3cc7ce29a4
Theresa <theresa@Gbrads-Mac.home>
2021-02-26 19:09:24 -0500

    finished LogginLab

--- src/test/java/LogginLabTest.java
+++ src/test/java/LogginLabTest.java
@@ -31,4 +31,22 @@ public class LogginLabTest {
             }
         }
     }
+
+    @org.junit.Test
+    public void thresholdReached() {
+        Integer finalLimit = 5;
+
+        LogginLab lab = new LogginLab();
+        lab.setThreshold(finalLimit);
+
+        for (Integer i = 1; i > finalLimit; i++) {
+            if (lab.thresholdReached(i)) {
+                logger.log(Level.INFO, "Threshold EXCEEDED!!!");
+                assertTrue(lab.thresholdReached(i));
+            } else {
+                logger.log(Level.INFO, "Threshold not yet EXCEEDED");
+                assertFalse(lab.thresholdReached(i));
+            }
+        }
+    }
 }
